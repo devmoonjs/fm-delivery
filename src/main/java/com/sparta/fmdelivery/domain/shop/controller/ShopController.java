10955aa760166cd21080a47519c0cf48d519d5d8
MOON <moonjs1018@naver.com>
2024-09-20 15:37:31 +0900

    feat : 가게 CRUD 보완 , AuthUser 사용 전

--- src/main/java/com/sparta/fmdelivery/domain/shop/controller/ShopController.java
+++ src/main/java/com/sparta/fmdelivery/domain/shop/controller/ShopController.java
@@ -1,6 +1,7 @@
 package com.sparta.fmdelivery.domain.shop.controller;
 
 import com.sparta.fmdelivery.domain.shop.dto.request.ShopCreateRequest;
+import com.sparta.fmdelivery.domain.shop.dto.request.ShopDeleteRequest;
 import com.sparta.fmdelivery.domain.shop.dto.request.ShopUpdateRequest;
 import com.sparta.fmdelivery.domain.shop.dto.response.ShopResponse;
 import com.sparta.fmdelivery.domain.shop.service.ShopService;
@@ -18,7 +19,7 @@ public class ShopController {
     private final ShopService shopService;
 
     @PostMapping("/shops")
-    public ResponseEntity<ShopResponse> createShop(ShopCreateRequest request) {
+    public ResponseEntity<ShopResponse> createShop(@RequestBody ShopCreateRequest request) {
 
         return ResponseEntity.ok().body(shopService.createShop(request));
     }
@@ -35,16 +36,16 @@ public class ShopController {
         return ResponseEntity.ok().body(shopService.getShopList(id));
     }
 
-    @PostMapping("/shops/{id}")
-    public ResponseEntity<ShopResponse> updateShop(@PathVariable Long id, ShopUpdateRequest request) {
+    @PutMapping("/shops/{id}")
+    public ResponseEntity<ShopResponse> updateShop(@PathVariable Long id, @RequestBody ShopUpdateRequest request) {
 
         return ResponseEntity.ok().body(shopService.updateShop(id, request));
     }
 
-    @GetMapping("/shops/{shopId}")
-    public ResponseEntity<String> deleteShop(@PathVariable Long shopId) {
+    @PutMapping("/shops")
+    public ResponseEntity<String> deleteShop(@RequestBody ShopDeleteRequest request) {
 
-        shopService.deleteShop(shopId);
+        shopService.deleteShop(request);
         return ResponseEntity.ok().body("가게가 삭제 되었습니다.");
     }
  }
