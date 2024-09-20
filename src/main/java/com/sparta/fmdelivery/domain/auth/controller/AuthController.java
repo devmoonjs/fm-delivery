eca4fb1bf965fc919b8e7a20a142896627f8b153
Sangmin Lee <sart5534@gmail.com>
2024-09-20 15:01:35 +0900

    feat : AuthUserArgumentResolver

--- src/main/java/com/sparta/fmdelivery/domain/auth/controller/AuthController.java
+++ src/main/java/com/sparta/fmdelivery/domain/auth/controller/AuthController.java
@@ -7,10 +7,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class AuthController {
 
     private final AuthService authService;
---
+++ src/main/java/com/sparta/fmdelivery/domain/common/dto/AuthUser.java
@@ -0,0 +1,18 @@
+package com.sparta.fmdelivery.domain.common.dto;
+
+import com.sparta.fmdelivery.domain.user.enums.UserRole;
+import lombok.Getter;
+
+@Getter
+public class AuthUser {
+
+    private final Long id;
+    private final String email;
+    private final UserRole userRole;
+
+    public AuthUser(Long id, String email, UserRole userRole) {
+        this.id = id;
+        this.email = email;
+        this.userRole = userRole;
+    }
+}
