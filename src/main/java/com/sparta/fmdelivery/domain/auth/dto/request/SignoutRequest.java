b4d8e6449d8acdff1f326f6924bdb14f03eec93d
Sangmin Lee <sart5534@gmail.com>
2024-09-20 19:36:00 +0900

    feat : 회원 탈퇴

---
+++ src/main/java/com/sparta/fmdelivery/domain/auth/dto/request/SignoutRequest.java
@@ -0,0 +1,15 @@
+package com.sparta.fmdelivery.domain.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignoutRequest {
+
+    @NotBlank
+    private String password;
+}
