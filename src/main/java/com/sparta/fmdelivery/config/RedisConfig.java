30ad0ee739a33b5b3a5c96fa78650d5de51b83e8
vieve <171054334+jay1864@users.noreply.github.com>
2024-09-23 12:59:00 +0900

    chore: redis config 수정

--- src/main/java/com/sparta/fmdelivery/config/RedisConfig.java
+++ src/main/java/com/sparta/fmdelivery/config/RedisConfig.java
@@ -1,18 +1,12 @@
 package com.sparta.fmdelivery.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@RequiredArgsConstructor
 @Configuration
-@EnableRedisRepositories  // Redis Repository 활성화
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String redisHost;
@@ -26,16 +20,4 @@ public class RedisConfig {
         return new LettuceConnectionFactory(redisHost, redisPort);  // .properties 파일에서 설정한 host, port 가져와 연동
     }
 
-    // Redis에 데이터 저장, 검색을 위한 RedisTemplate 설정
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-
-        // StringRedisSerializer로 직렬화 방법 설정
-        redisTemplate.setKeySerializer(new StringRedisSerializer());    // 문자열 key 저장
-        redisTemplate.setValueSerializer(new StringRedisSerializer());  // 문자열 value 저장
-
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
-    }
 }
