34b8943d744f890c9b076518235a436db21330b0
vieve <171054334+jay1864@users.noreply.github.com>
2024-09-23 11:26:23 +0900

    feat: 주문, 장바구니 기능 관련 Controller 수정 및 추가

--- src/main/java/com/sparta/fmdelivery/domain/order/controller/OrderController.java
+++ src/main/java/com/sparta/fmdelivery/domain/order/controller/OrderController.java
@@ -9,7 +9,6 @@ import com.sparta.fmdelivery.domain.order.dto.response.OrderListResponse;
 import com.sparta.fmdelivery.domain.order.dto.response.OrderResponse;
 import com.sparta.fmdelivery.domain.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,23 +20,27 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    // 사용자 - 주문 요청
     @PostMapping
     public ApiResponse<OrderResponse> createOrder(@Auth AuthUser authUser,
                                                   @RequestBody OrderRequest orderRequest) {
         return ApiResponse.onSuccess(orderService.saveOrder(authUser, orderRequest));
     }
 
+    // 사장 - 주문 수락
     @PatchMapping("/{orderId}")
     public ApiResponse<OrderResponse> acceptOrder(@Auth AuthUser authUser,
                                                      @PathVariable Long orderId) {
         return ApiResponse.onSuccess(orderService.updateOrder(authUser, orderId));
     }
 
+    // 주문 목록 조회
     @GetMapping
     public ApiResponse<List<OrderListResponse>> getAllOrders(@Auth AuthUser authUser) {
         return ApiResponse.onSuccess(orderService.getAllOrders(authUser));
     }
 
+    // 주문 상세 조회
     @GetMapping("/{orderId}")
     public ApiResponse<OrderDetailResponse> getOrder(@Auth AuthUser authUser,
                                                         @PathVariable Long orderId) {
